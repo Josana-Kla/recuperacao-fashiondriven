dfedf45d15337a352fa7915d24b15f5aa0a80390
Josana-Kla <josana.0205@gmail.com>
2022-07-06 10:24:22 -0300

    feat: created prompt to get user name

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* let userName = prompt('Qual é o seu nome?');
+
+if(userName === "undefined" || userName === "" || Number(userName)) {
+    alert('Digite um nome válido');
+    window.location.reload();
+}
+ */
+
+let userName = "Maria";
+
+
