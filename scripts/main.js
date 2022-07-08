3687081218f7198ec1571c91a4f287dc84a1b3c3
Josana-Kla <josana.0205@gmail.com>
2022-07-08 12:36:35 -0300

    feat: changing order names to English and initialized function finish order

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,6 @@ function selectModel(element) {
     }
 
     element.classList.add('border-circle-blue');
-
 }
 
 function selectCollar(element) {
@@ -32,7 +31,6 @@ function selectCollar(element) {
     }
 
     element.classList.add('border-circle-blue');
-
 }
 
 function selectTissue(element) {
@@ -44,20 +42,19 @@ function selectTissue(element) {
     }
 
     element.classList.add('border-circle-blue');
-
-    /* collarSelectedByUser = element.nextElementSibling.innerHTML;
-    console.log(collarSelectedByUser) */
-
 }
-
+let imageReferenceUrl;
+let orderIsAllRight;
 function turnsBlueButton() {
     let catchValueImageReference = document.querySelector('.big-line input');
-    let catchButton = document.querySelector('.big-line button');
-    let linkIsValid = catchValueImageReference.value.startsWith('https://') || catchValueImageReference.value.startsWith('http:/');
-    let linkIsAnImage = catchValueImageReference.value.includes('.jpg') || catchValueImageReference.value.includes('.jpeg') || catchValueImageReference.value.includes('.svg') || catchValueImageReference.value.includes('.png') || catchValueImageReference.value.includes('.bmp') || catchValueImageReference.value.includes('.tiff') || catchValueImageReference.value.includes('.raw');
+    const catchButton = document.querySelector('.big-line button');
+    const linkIsValid = catchValueImageReference.value.startsWith('https://') || catchValueImageReference.value.startsWith('http:/');
+    const linkIsAnImage = catchValueImageReference.value.includes('.jpg') || catchValueImageReference.value.includes('.jpeg') || catchValueImageReference.value.includes('.svg') || catchValueImageReference.value.includes('.png') || catchValueImageReference.value.includes('.bmp') || catchValueImageReference.value.includes('.tiff') || catchValueImageReference.value.includes('.raw');
    
     if(modelSelectedByUser !== undefined && collarSelectedByUser !== undefined && tissueSelectedByUser !== undefined && catchValueImageReference.value !== "" && linkIsValid && linkIsAnImage) {
         catchButton.classList.add('button-blue');
+        imageReferenceUrl = catchValueImageReference.value;
+        orderIsAllRight = true;
     } else {
         catchValueImageReference.value = "";
         catchButton.classList.remove('button-blue');
@@ -65,10 +62,86 @@ function turnsBlueButton() {
     }
 }
 
-
-function finishOrder() {
-    //se der certo - alert confirmando a encomenda; e envia p servidor
-    //se der errado - alert "Ops, não conseguimos processar sua encomenda"
-    
-
+function changeModelNamesToEnglish() {
+    if(modelSelectedByUser === "T-shirt") {
+        modelSelectedByUser = modelSelectedByUser.toLowerCase();
+    } else if(modelSelectedByUser === "Camiseta") {
+        modelSelectedByUser = "top-tank";
+    } else if(modelSelectedByUser === "Manga longa") {
+        modelSelectedByUser = "long";
+    }
 }
+
+function changeCollarNamesToEnglish() {
+    if(collarSelectedByUser === "Gola V") {
+        collarSelectedByUser = "v-neck";
+    } else if(collarSelectedByUser === "Gola redonda") {
+        collarSelectedByUser = "round";
+    } else if(collarSelectedByUser === "Gola polo") {
+        collarSelectedByUser = "polo";
+    }
+}
+
+function changeTissueNamesToEnglish() {
+    if(tissueSelectedByUser === "Seda") {
+        tissueSelectedByUser = "silk";
+    } else if(tissueSelectedByUser === "Algodão") {
+        tissueSelectedByUser = "cotton";
+    } else if(tissueSelectedByUser === "Poliéster") {
+        tissueSelectedByUser = "polyester";
+    }
+}
+
+/* let objectModelToOrder = { 
+    `
+        model: ${modelSelectedByUser},
+        neck: ${collarSelectedByUser},
+        material: ${tissueSelectedByUser},
+        image: ${imageReferenceUrl},
+        owner: "oii",
+        author: "euu"
+    `
+} */
+
+
+
+
+let objectModelToOrder;
+
+
+function finishOrder(element) {
+    if(orderIsAllRight === true) {
+        
+        
+        alert(`
+            Encomenda confirmada com sucesso!
+            Você pediu:
+            - Modelo: ${modelSelectedByUser},
+            - Gola: ${collarSelectedByUser},
+            - Tecido: ${tissueSelectedByUser}
+        `)
+
+        changeModelNamesToEnglish();
+        changeCollarNamesToEnglish();
+        changeTissueNamesToEnglish();
+
+        objectModelToOrder = {  
+            "model": modelSelectedByUser,
+            "neck": collarSelectedByUser,
+            "material": tissueSelectedByUser,
+            "image": imageReferenceUrl,
+            "owner": userName,
+            "author": "nonee" 
+        }
+
+        console.log(objectModelToOrder);
+        //manda p servidor
+    }
+    //se der certo - alert confirmando a encomenda; e envia p servidor
+    //se der errado - alert "Ops, não conseguimos processar sua encomenda" 
+}
+
+
+
+
+
